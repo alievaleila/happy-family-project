dff2ef290121dcaa2eb687fc1fdc3004802b7fd9
Saleh Samadli <realmadrid.rm171@gmail.com>
2024-10-09 22:10:13 +0400

    hw01-happy-family-project attached

---
+++ src/main/java/az/edu/turing/happyfamilyv1/Family.java
@@ -0,0 +1,55 @@
+package az.edu.turing.happyfamilyv1;
+
+public class Family {
+    private Human mother;
+    private Human father;
+    private Human children;
+    private Pet pet;
+
+    public Family(Human mother, Human father) {
+        this.mother = mother;
+        this.father = father;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human getChildren() {
+        return children;
+    }
+
+    public void setChildren(Human children) {
+        this.children = children;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "mother=" + mother +
+                ", father=" + father +
+                ", children=" + children +
+                ", pet=" + pet +
+                '}';
+    }
+}
