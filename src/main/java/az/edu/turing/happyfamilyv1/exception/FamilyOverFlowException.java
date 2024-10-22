5bf5df76c76cda610f3bf2a2b53c84b86dc43606
alievaleila <meliyeva452@gmail.com>
2024-10-22 18:04:44 +0400

    some changes about exception

--- src/main/java/az/edu/turing/happyfamilyv1/exception/FamilyOverFlowException.java
+++ src/main/java/az/edu/turing/happyfamilyv1/exception/FamilyOverFlowException.java
@@ -5,13 +5,5 @@ public class FamilyOverFlowException extends RuntimeException {
     public FamilyOverFlowException(String message) {
         super(message);
     }
-
-    public FamilyOverFlowException(Throwable cause) {
-        super(cause);
-    }
-
-    public FamilyOverFlowException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
 
